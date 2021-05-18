f2a290eb13083a77eab354d193cbddffbaf2c10e
Matin Sarahi <matinsarahi@gmail.com>
2021-05-18 14:24:08 -0400

    api

---
+++ src/api.js
@@ -0,0 +1,36 @@
+const base_url = "https://api.rawg.io/api/";
+
+const key = "?key=679444e92c77420799afc35b706fc24d";
+
+//current date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//popular games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+const popularGamesURL = () => `${base_url}${popular_games}${key}`;
+
+console.log(popularGamesURL());
