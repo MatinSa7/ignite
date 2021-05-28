dd2355bc0008ca00a61f3bba1f8e14583e40d8e1
Matin Sarahi <matinsarahi@gmail.com>
2021-05-28 12:56:06 -0400

    image optimizing

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { smallImage } from "../util";
 
 const GameDetail = () => {
   const history = useHistory();
@@ -34,14 +35,18 @@ const GameDetail = () => {
               </Info>
             </Stats>
             <Media>
-              <img src={game.background_image} alt="image" />
+              <img src={smallImage(game.background_image, 1280)} alt="image" />
             </Media>
             <Description>
               <p>{game.description_raw}</p>
             </Description>
             <div className="gallery">
               {screen.results.map((screen) => (
-                <img key={screen.id} src={screen.image} alt="gameImage" />
+                <img
+                  key={screen.id}
+                  src={smallImage(screen.image, 1280)}
+                  alt="gameImage"
+                />
               ))}
             </div>
           </Detail>
