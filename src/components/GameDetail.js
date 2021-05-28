2749c3f63c22833fce8f0878a2574cb5a5ecc47d
Matin Sarahi <matinsarahi@gmail.com>
2021-05-28 13:56:09 -0400

    card Animation

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { smallImage } from "../util";
 
-const GameDetail = () => {
+const GameDetail = ({ pathId }) => {
   const history = useHistory();
   const exitDetailHandler = (e) => {
     const element = e.target;
@@ -15,14 +15,15 @@ const GameDetail = () => {
     }
   };
   const { screen, game, isLoading } = useSelector((state) => state.detail);
+  console.log(typeof pathId + pathId);
   return (
     <>
       {!isLoading && (
         <CardShadow className="shadow" onClick={exitDetailHandler}>
-          <Detail>
+          <Detail layoutId={pathId}>
             <Stats>
               <div className="rating">
-                <h3>{game.name}</h3>
+                <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
                 <p>Rating: {game.rating}</p>
               </div>
               <Info>
@@ -35,7 +36,11 @@ const GameDetail = () => {
               </Info>
             </Stats>
             <Media>
-              <img src={smallImage(game.background_image, 1280)} alt="image" />
+              <motion.img
+                layoutId={`image ${pathId}`}
+                src={smallImage(game.background_image, 1280)}
+                alt="image"
+              />
             </Media>
             <Description>
               <p>{game.description_raw}</p>
@@ -64,6 +69,7 @@ const CardShadow = styled(motion.div)`
   position: fixed;
   top: 0;
   left: 0;
+  z-index: 1000;
   &::-webkit-scrollbar {
     width: 0.5rem;
   }
